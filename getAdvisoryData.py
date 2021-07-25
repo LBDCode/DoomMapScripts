7755fd81c99818bc72aed380ac87d00c8e2051a6
LBDCode <lduggan@espassociates.com>
2021-07-25 12:05:25 -0400

    cleanup

--- getAdvisoryData.py
+++ getAdvisoryData.py
@@ -1,7 +1,11 @@
 import requests
 import tarfile
-
 import io
+import os
+import subprocess
+import psycopg2
+from dotenv import load_dotenv
+
 
 
 def getAdvisoryData():
@@ -10,9 +14,8 @@ def getAdvisoryData():
 
     url = 'https://tgftp.nws.noaa.gov/SL.us008001/DF.sha/DC.cap/DS.WWA/current_all.tar.gz'
 
-    print('Downloading shapefile...')
-    
-    target_path = 'current_all.tar.gz'
+  
+    target_path = 'advisoryData/current_all.tar.gz'
 
     response = requests.get(url, stream=True)
     if response.status_code == 200:
@@ -20,11 +23,35 @@ def getAdvisoryData():
             f.write(response.raw.read())
 
     tf = tarfile.open(target_path)
-    tf.extractall()
-
-    print("Done")
+    tf.extractall('./advisoryData')
 
 
+
+def updateDatabase():
+    
+    load_dotenv()
+
+    DB_NAME = os.getenv('DB_NAME')
+    PORT = os.getenv('PORT')
+    USER = os.getenv('USER')
+    PASSWORD = os.getenv('PASSWORD')
+    HOST = os.getenv('HOST')
+    AREA_TABLE=os.getenv('AREA_TABLE')
+
+
+    conn = psycopg2.connect(dbname=DB_NAME, port=PORT, user=USER,password=PASSWORD, host=HOST)
+    cur = conn.cursor()
+
+    cur.execute("DELETE FROM {0}".format(AREA_TABLE))
+
+    conn.commit()
+    conn.close()
+    
+
+    cmd = 'shp2pgsql -a ./advisoryData/current_all.shp public.{0} | psql -q -h localhost -d {1} -U {2}'.format(AREA_TABLE, DB_NAME, USER)
+
+    subprocess.call(cmd, shell=True)
 
 
 getAdvisoryData()
+updateDatabase()
